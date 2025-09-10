a70573c479023d7b855a7fe40d4f460f48bbd4f7
Cache Angus <cacangus@gmail.com>
2025-09-10 18:51:09 -0400

    env fix up

--- packages/src/config/db.js
+++ packages/src/config/db.js
@@ -5,10 +5,7 @@ dotenv.config({ path: [`.env`, `.env.local`] });
 
 export const connectDB = async () => {
   try {
-    console.log(process.env);
-    await mongoose.connect(
-      "mongodb+srv://cacangus_db_user:MdrEVNBAbbbuest0@cluster0.l127akw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("mongoose connected");
   } catch (error) {
     console.log("MongoDB bad authentication");
